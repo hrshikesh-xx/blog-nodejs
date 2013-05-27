db6bfd4440455db05d752c1c0c1af00414af08b6
Hrshikesha <hrshikesha@hrshikesha-Dell-System-XPS-L702X.(none)>
2013-05-27 20:10:56 +0200

    updated

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var blog = require('./model');
+var blog = require('./lib/model');
+//var db = require('./lib/db');
+var routes = require('./lib/routes');
+var lib = require('./lib/lib');
 
 var app = express();
 
 app.configure(function() {
 	app.set('port', process.env.PORT || 3000);
-	app.set('views', __dirname + '/views');
+//	app.set('views', __dirname + '/views');
+	app.set('views', __dirname + '/views/article');
 	app.set('view engine', 'jade');
 	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({secret: 'secretpasswordforsessions', store: lib.getSessionStore()
+}));
 	app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.get('/', blog.findAll);
+//app.get('/', blog.findAll);
+
+app.get('/', blog.article);
 
 app.get('/blog', blog.findAll);
 app.get('/blog/new', blog.newBlog);
@@ -25,6 +34,13 @@ app.post('/updateBlog', blog.updateBlog);
 app.post('/deleteBlog', blog.deleteBlog);
 app.post('/blog/addComment', blog.addCommentToBlog);
 
-http.createServer(app).listen(app.get('port'), function() {
+app.get('/api/user/:username', routes.getUser);
+app.post('/signup', routes.signup);
+app.post('/login', routes.login);
+
+var server = http.createServer(app);
+
+server.listen(app.get('port'), function() {
 	console.log('Listening on port: ' + app.get('port'));
+	lib.createSocket(server);
 });
